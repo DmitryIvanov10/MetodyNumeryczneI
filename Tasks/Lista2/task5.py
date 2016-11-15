ff955303a49782dccd074e09d41b92f32c15da0b
Dmitry Ivanov <dmitry.ivanov.zagranju@gmail.com>
2016-11-15 12:33:43 +0100

    small changes

--- Tasks/Lista2/task5.py
+++ Tasks/Lista2/task5.py
@@ -13,31 +13,31 @@ def plotWithMatplotlib(x, y, name):
     plt.xlabel('x')
 
 # A function for exponent
-def exponent(x, N):
+def expUncertainty(x, N):
     sum = 0
     partSums = []
-    for i in range (N):
+    for i in range (N+1):
         sum += (x ** i) / factorial(i)
-        partSums.append(abs(exp(x) - sum)/exp(x))
+        partSums.append(abs(sum - exp(x))/exp(x))
     return partSums
 
 # Amount of partial sums variables
 N = 60
-NValues = arange(N)
+NValues = arange(N+1)
 
 x = 10
-plotWithMatplotlib(NValues, exponent(x, N), 'Exp uncertainty for |x| = 10')
+plotWithMatplotlib(NValues, expUncertainty(x, N), 'Exp uncertainty for |x| = 10')
 
 x = -10
-plotWithMatplotlib(NValues, exponent(x, N), 'Exp uncertainty for |x| = 10')
+plotWithMatplotlib(NValues, expUncertainty(x, N), 'Exp uncertainty for |x| = 10')
 
 plt.show()
 plt.clf()
 
 x = 2
-plotWithMatplotlib(NValues, exponent(x, N), 'Exp uncertainty for |x| = 2')
+plotWithMatplotlib(NValues, expUncertainty(x, N), 'Exp uncertainty for |x| = 2')
 
 x = -2
-plotWithMatplotlib(NValues, exponent(x, N), 'Exp uncertainty for |x| = 2')
+plotWithMatplotlib(NValues, expUncertainty(x, N), 'Exp uncertainty for |x| = 2')
 
 plt.show()

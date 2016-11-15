3d4cd9360470c0e9213d82900c2a3754d7a670ff
Dmitry Ivanov <dmitry.ivanov.zagranju@gmail.com>
2016-11-15 13:12:49 +0100

    Last changes to lista2

--- Tasks/Lista2/task5.py
+++ Tasks/Lista2/task5.py
@@ -8,25 +8,27 @@ import matplotlib.pyplot as plt
 # Plot with MatPlotLib by x and y
 def plotWithMatplotlib(x, y, name):
     plt.plot(x,y)
-
     plt.ylabel(name)
     plt.xlabel('x')
 
 # A function for exponent
 def expUncertainty(x, N):
     sum = 0
-    partSums = []
+    uncertainties = []
     for i in range (N+1):
         sum += (x ** i) / factorial(i)
-        partSums.append(abs(sum - exp(x))/exp(x))
-    return partSums
+        # show values of exponent and partial sums if needed
+        # print ("N = {}, exp = {}, sum = {}".format(i, exp(x), sum))
+        uncertainties.append(abs(sum - exp(x))/exp(x))
+    return uncertainties
 
 # Amount of partial sums variables
 N = 60
 NValues = arange(N+1)
 
 x = 10
-plotWithMatplotlib(NValues, expUncertainty(x, N), 'Exp uncertainty for |x| = 10')
+plotWithMatplotlib(NValues, expUncertainty(x, N), 'Exp uncertainty for x = 10')
+plt.show()
 
 x = -10
 plotWithMatplotlib(NValues, expUncertainty(x, N), 'Exp uncertainty for |x| = 10')
